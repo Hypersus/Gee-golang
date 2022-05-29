3784c9f0ea26e622257b4abdb4780a7ae9712ae0
zeke <hypersus@outlook.com>
2022-05-29 22:17:27 +0800

    [Gee-Day3] Use trie tree to implement dynamical routing

---
+++ day3-router/main.go
@@ -0,0 +1,42 @@
+
+
+//===================================================================#
+//	Copyright (C) 2022 Zeke. All rights reserved
+// 
+//	Filename:		main.go
+//	Author:			Zeke
+//	Date:			2022.05.29
+//	E-mail:			hypersus@outlook.com
+//	Discription:	test script
+//	
+//===================================================================#
+
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=zeke
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/zeke
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
+
+	r.Run(":9999")
+}
